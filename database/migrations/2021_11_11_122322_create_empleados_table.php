da10da0a7ffa46a14afe40a650fc73c5a844ec4b
Jose <chino_wilde@hotmail.com>
2021-11-11 09:53:54 -0300

    terminando las migraciones

--- database/migrations/2021_11_11_122322_create_empleados_table.php
+++ database/migrations/2021_11_11_122322_create_empleados_table.php
@@ -15,6 +15,13 @@ class CreateEmpleadosTable extends Migration
     {
         Schema::create('empleados', function (Blueprint $table) {
             $table->id();
+
+            $table->string('Nombre');
+            $table->string('ApellidoPaterno');
+            $table->string('ApellidoMaterno');
+            $table->string('Correo');
+            $table->string('Foto');
+            
             $table->timestamps();
         });
     }
