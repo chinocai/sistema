3fd541ecf844610e866638c1c03f79ad9bb047f3
Jose <chino_wilde@hotmail.com>
2021-11-12 13:11:55 -0300

    formulario de edicion

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -1,17 +1,18 @@
 
 <label for="Nombre"> Nombre </label>
-<input class="form-control" type="text" name="Nombre" id="Nombre">
+<input class="form-control" type="text" name="Nombre" value="{{ $empleado->Nombre }}" id="Nombre">
 <br>
 <label for="Nombre"> ApellidoMaterno </label>
-<input class="form-control" type="text" name="ApellidoPaterno" id="ApellidoPaterno">
+<input class="form-control" type="text" name="ApellidoPaterno" value="{{ $empleado->ApellidoPaterno }}" id="ApellidoPaterno">
 <br>
 <label for="Nombre"> ApellidoPaterno </label>
-<input class="form-control" type="text" name="ApellidoMaterno" id="ApellidoMaterno">
+<input class="form-control" type="text" name="ApellidoMaterno" value="{{ $empleado->ApellidoMaterno }}" id="ApellidoMaterno">
 <br>
 <label for="Nombre"> Correo </label>
-<input class="form-control" type="text" name="Correo" id="Correo">
+<input class="form-control" type="text" name="Correo" value="{{ $empleado->Correo }}" id="Correo">
 <br>
 <label for="Nombre"> Foto </label>
-<input class="form-control" type="file" name="Foto" id="Foto">
+{{ $empleado->Foto }}
+<input class="form-control" type="file" name="Foto" value="" id="Foto">
 <br>
 <input class="form-control" type="submit" value="Guardar datos" id="Enviar">
