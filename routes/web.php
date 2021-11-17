6154de5c345c30c9e9e6d3279077eefb83f12ee5
Jose <chino_wilde@hotmail.com>
2021-11-17 13:24:55 -0300

    configurando autenticacion

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,20 @@ use App\Http\Controllers\EmpleadoController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-Route::get('/empleado', function () {
+/* Route::get('/empleado', function () {
     return view('empleado.index');
 });
 
 Route::get('/empleado/create',[EmpleadoController::class, 'create']);
-
+*/
 Route::resource('empleado',EmpleadoController::class);
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [EmpleadoController::class, 'index'])->name('home');
+
+Route::group(['middleware' =>'auth'], function () {
+    Route::get('/', [EmpleadoController::class, 'index'])->name('home');
+});
